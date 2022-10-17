83ff7d056137a9373792ef73c33dc9f000e53e68
simhes <106815004+simhes@users.noreply.github.com>
2022-10-17 15:58:56 +0100

    test

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
-import numpy as np # import numpy, simplifies matrixes and linear algebra A LOT
+#import numpy as np # import numpy, simplifies matrixes and linear algebra A LOT  //Problem för mig med denna rad.
 import math # imports math library, holds a lot of essential math 
 # This file will take data from our dataset and train an algorithm by K-nearest-neighbors algorithm
 
-
+print("hej")
