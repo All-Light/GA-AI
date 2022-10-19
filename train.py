fa0ae4169277f8ccb4ae35e9b96a27bccf4e311d
Alexander Andersson <alexander1868@hotmail.com>
2022-10-19 14:12:50 +0200

    Added datasets

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
-#import numpy as np # import numpy, simplifies matrixes and linear algebra A LOT  //Problem för mig med denna rad.
+import numpy as np # import numpy, simplifies matrixes and linear algebra A LOT  //Problem för mig med denna rad.
 import math # imports math library, holds a lot of essential math 
 # This file will take data from our dataset and train an algorithm by K-nearest-neighbors algorithm
 
 print("hej")
+a = input("test")
