ad8e2b77f151a6246bde006b626e73e9918cfcc4
Alexander Andersson <alexander1868@hotmail.com>
2022-10-26 14:14:14 +0200

    changed virtual env

--- train.py
+++ train.py
@@ -1,6 +1,13 @@
 import numpy as np # import numpy, simplifies matrixes and linear algebra A LOT  //Problem för mig med denna rad.
 import math # imports math library, holds a lot of essential math 
 # This file will take data from our dataset and train an algorithm by K-nearest-neighbors algorithm
+# [ 1 2 3 4
+#   1 2 3 4
+#   1 2 3 4]
 
-print("hej")
-a = input("test")
+a = np.zeros([1,2])
+a[0] = input("skriv ett tal: ")
+print(a)
+b=np.cos(a)
+
+print("cos ur talet är: " , b)
